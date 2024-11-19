ed521d6b5b40c0ddee6b42314b2980a1f0eb5342
PHAMTIENHUNG772006 <hungpham772006@gmail.com>
2024-11-19 21:54:01 +0700

    Add files via upload

---
+++ tinhChuvidientichhinhtron.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	float pi, r, chuVi, dienTich;
+	r = 3.5;
+	pi = 3.14;
+	chuVi = (r * 2) * pi;
+	dienTich = r * r * pi;
+	printf("chu vi cua hinh tron la : %d\n", chuVi);
+	printf("dien tich cua hinh tron la : %d", dienTich);
+	
+	return 0;
+	
+}
+
