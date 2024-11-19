d13c2b4506a0e921530f3a4b7caf59405935bd32
PHAMTIENHUNG772006 <hungpham772006@gmail.com>
2024-11-19 21:51:41 +0700

    Add files via upload

---
+++ tinhChuvidientichhinhvuong.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a, chuVi, dienTich;
+	a = 5;
+	chuVi = a * 4;
+	dienTich = a * a;
+	printf("chu vi cua hinh vuong la : %d\n", chuVi);
+	printf("dien tich cua hinh vuong la : %d", dienTich);
+	
+	return 0;
+	
+}
+
